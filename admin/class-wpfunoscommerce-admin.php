285e4483439f9023e52d068d003dfdbafcb7a4f5
efraim <efraim@efraim.cat>
2022-07-23 20:13:55 +0200

    V1.0.0 23-07-22 20:13

--- admin/class-wpfunoscommerce-admin.php
+++ admin/class-wpfunoscommerce-admin.php
@@ -1,52 +1,52 @@
 <?php
 
 /**
- * The admin-specific functionality of the plugin.
- *
- * @link       https://efraim.cat
- * @since      1.0.0
- *
- * @package    Wpfunoscommerce
- * @subpackage Wpfunoscommerce/admin
- */
+* The admin-specific functionality of the plugin.
+*
+* @link       https://efraim.cat
+* @since      1.0.0
+*
+* @package    Wpfunoscommerce
+* @subpackage Wpfunoscommerce/admin
+*/
 
 /**
- * The admin-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
- * @package    Wpfunoscommerce
- * @subpackage Wpfunoscommerce/admin
- * @author     Efraim Bayarri <efraim@efraim.cat>
- */
+* The admin-specific functionality of the plugin.
+*
+* Defines the plugin name, version, and two examples hooks for how to
+* enqueue the admin-specific stylesheet and JavaScript.
+*
+* @package    Wpfunoscommerce
+* @subpackage Wpfunoscommerce/admin
+* @author     Efraim Bayarri <efraim@efraim.cat>
+*/
 class Wpfunoscommerce_Admin {
 
 	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
+	* The ID of this plugin.
+	*
+	* @since    1.0.0
+	* @access   private
+	* @var      string    $plugin_name    The ID of this plugin.
+	*/
 	private $plugin_name;
 
 	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
+	* The version of this plugin.
+	*
+	* @since    1.0.0
+	* @access   private
+	* @var      string    $version    The current version of this plugin.
+	*/
 	private $version;
 
 	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
+	* Initialize the class and set its properties.
+	*
+	* @since    1.0.0
+	* @param      string    $plugin_name       The name of this plugin.
+	* @param      string    $version    The version of this plugin.
+	*/
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
@@ -55,49 +55,119 @@ class Wpfunoscommerce_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
+	* Register the stylesheets for the admin area.
+	*
+	* @since    1.0.0
+	*/
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wpfunoscommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wpfunoscommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wpfunoscommerce-admin.css', array(), $this->version, 'all' );
 
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
+	* Register the JavaScript for the admin area.
+	*
+	* @since    1.0.0
+	*/
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wpfunoscommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wpfunoscommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wpfunoscommerce-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
+	/*********************************/
+	/*****  UTILIDADES          ******/
+	/*********************************/
+	/**
+	* Utility: log files.
+	*/
+	private function logFiles()
+	{
+		$upload_dir = wp_upload_dir();
+		$files = scandir( $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs');
+		?>
+		<form action="" method="post">
+			<ul>
+				<?php foreach ( $files as $file ) { ?>
+					<?php if( substr( $file , -4) == '.log'){?>
+						<li><input type="radio" id="age[]" name="logfile" value="<?php esc_html_e( $file ); ?>">
+							<?php esc_html_e( $file . ' -> ' . date("d-m-Y H:i:s", filemtime( $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs/' . $file  ) ) ); ?>
+						</li>
+					<?php }?>
+				<?php }?>
+			</ul>
+			<div class="wpfunos-send-logfile">
+				<input type="submit" value="<?php _e( 'Ver archivo de registro', 'wpfunos' ); ?>">
+			</div>
+		</form>
+		<?php
+	}
+	/**
+	* Utility: show log file.
+	*/
+	private function showLogFile()
+	{
+		$upload_dir = wp_upload_dir();
+		if (isset($_POST['logfile'])) {
+			?>
+			<hr />
+			<h3><?php esc_html_e( $_POST['logfile'] ); ?> </h3>
+			<textarea id="wpfunoslogfile" name="wpfunoslogfile" rows="30" cols="180" readonly>
+				<?php esc_html_e( ( file_get_contents( $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs/' . $_POST['logfile'] ) ) ); ?>
+			</textarea>
+			<?php
+		}
+	}
+	/**
+	* Utility: create entry in the log file.
+	*/
+	private function custom_logs($message){
+		$upload_dir = wp_upload_dir();
+		if (is_array($message)) {
+			$message = json_encode($message);
+		}
+		if (!file_exists( $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs') ) {
+			mkdir( $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs' );
+		}
+		$time = current_time("d-M-Y H:i:s");
+		$ban = "#$time: $message\r\n";
+		$file = $upload_dir['basedir'] . '/' . $this->plugin_name . '-logs/' . $this->plugin_name .'-adminlog-' . current_time("Y-m-d") . '.log';
+		$open = fopen($file, "a");
+		fputs($open, $ban);
+		fclose( $open );
+	}
+
+	/**
+	* GZIPs a file on disk (appending .gz to the name)
+	*
+	* From http://stackoverflow.com/questions/6073397/how-do-you-create-a-gz-file-using-php
+	* Based on function by Kioob at:
+	* http://www.php.net/manual/en/function.gzwrite.php#34955
+	*
+	* @param string $source Path to file that should be compressed
+	* @param integer $level GZIP compression level (default: 9)
+	* @return string New filename (with .gz appended) if success, or false if operation fails
+	*/
+	private function gzCompressFile($source, $level = 9){
+		$dest = $source . '.gz';
+		$mode = 'wb' . $level;
+		$error = false;
+		if ($fp_out = gzopen($dest, $mode)) {
+			if ($fp_in = fopen($source,'rb')) {
+				while (!feof($fp_in))
+				gzwrite($fp_out, fread($fp_in, 1024 * 512));
+				fclose($fp_in);
+			} else {
+				$error = true;
+			}
+			gzclose($fp_out);
+		} else {
+			$error = true;
+		}
+		if ($error)
+		return false;
+		else
+		return $dest;
+	}
 }
--- includes/class-wpfunoscommerce.php
+++ includes/class-wpfunoscommerce.php
@@ -78,8 +78,8 @@ class Wpfunoscommerce {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
-		//$this->define_paycomet_hooks();
-		//$this->define_frakmenta_hooks();
+		$this->define_paycomet_hooks();
+		$this->define_frakmenta_hooks();
 
 	}
 
@@ -127,12 +127,12 @@ class Wpfunoscommerce {
 		/**
 		* frakmenta
 		*/
-		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'frakmenta/class-wpc-frakmenta.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'frakmenta/class-wpfc-frakmenta.php';
 
 		/**
 		* paycomet
 		*/
-		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'paycomet/class-wpc-paycomet.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'paycomet/class-wpfc-paycomet.php';
 
 		$this->loader = new Wpfunoscommerce_Loader();
 
@@ -194,14 +194,14 @@ class Wpfunoscommerce {
 	* @since    1.0.0
 	* @access   private
 	*/
-	//private function define_frakmenta_hooks() {
+	private function define_frakmenta_hooks() {
 
-		//$plugin_frakmenta = new Wpfc_Frakmenta( $this->get_plugin_name(), $this->get_version() );
+		$plugin_frakmenta = new Wpfc_Frakmenta( $this->get_plugin_name(), $this->get_version() );
 
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_frakmenta, 'enqueue_styles' );
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_frakmenta, 'enqueue_scripts' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_frakmenta, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_frakmenta, 'enqueue_scripts' );
 
-	//}
+	}
 
 	/**
 	* Register all of the hooks paycomet
@@ -210,14 +210,14 @@ class Wpfunoscommerce {
 	* @since    1.0.0
 	* @access   private
 	*/
-	//private function define_paycomet_hooks() {
+	private function define_paycomet_hooks() {
 
-		//$plugin_paycomet = new Wpfc_Paycomet( $this->get_plugin_name(), $this->get_version() );
+		$plugin_paycomet = new Wpfc_Paycomet( $this->get_plugin_name(), $this->get_version() );
 
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_paycomet, 'enqueue_styles' );
-		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_paycomet, 'enqueue_scripts' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_paycomet, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_paycomet, 'enqueue_scripts' );
 
-	//}
+	}
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
