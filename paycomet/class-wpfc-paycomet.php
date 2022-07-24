97413e2ec87954716e81e078b273f3e9f16ed796
efraim <efraim@efraim.cat>
2022-07-24 11:11:19 +0200

    V1.0.0 24-07-22 11:11

--- paycomet/class-wpfc-paycomet.php
+++ paycomet/class-wpfc-paycomet.php
@@ -1,103 +1,49 @@
 <?php
 
 /**
- * The public-facing functionality of the plugin.
- *
- * @link       https://efraim.cat
- * @since      1.0.0
- *
- * @package    Wpfunoscommerce
- * @subpackage Wpfunoscommerce/public
- */
-
-/**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the public-facing stylesheet and JavaScript.
- *
- * @package    Wpfunoscommerce
- * @subpackage Wpfunoscommerce/public
- * @author     Efraim Bayarri <efraim@efraim.cat>
- */
+* The public-facing functionality of the plugin.
+*
+* @link       https://efraim.cat
+* @since      1.0.0
+*
+* @package    Wpfunoscommerce
+* @subpackage Wpfunoscommerce/public
+*/
 class Wpfc_Paycomet {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
 	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
 	private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
 	public function __construct( $plugin_name, $version ) {
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
+		add_shortcode( 'wpfc-compra', array( $this, 'wpfcComprarProductoShortcode' ));
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wpfunoscommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wpfunoscommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wpfc-paycomet.css', array(), $this->version, 'all' );
+	}
 
+	public function enqueue_scripts() {
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wpfc-paycomet.js', array( 'jquery' ), $this->version, false );
 	}
 
 	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wpfunoscommerce_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wpfunoscommerce_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wpfc-paycomet.js', array( 'jquery' ), $this->version, false );
-
+	* Shortcode [wpfc-compra producto="84089"]
+	* /finalizar-compra?add-to-cart=84089
+	*
+	* Redirigir a finaliza compra
+	*/
+	public function wpfcComprarProductoShortcode( $atts, $content = "" ) {
+		$a = shortcode_atts( array(
+			'producto'=>'',
+		), $atts );
+		global $woocommerce;
+		$woocommerce->cart->empty_cart();
+		$wpf_checkout = $woocommerce->cart->get_checkout_url();
+		return $wpf_checkout.'?add-to-cart='.$a['producto'];
 	}
+
+
 
 }
